7c1d9136ff4d2e00589fee540bbc9c2e69f96bc8
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2018-06-12 20:58:09 +0200

    :heavy_plus_sign: Add web3

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,3 +39,7 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+const listener = app.listen(80, function(){
+    console.log('Listening on port ' + listener.address().port);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const web3 = require('web3');
+
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(web3);
   res.render('index', { title: 'Express' });
 });
 
